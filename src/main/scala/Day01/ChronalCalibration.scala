374d2b41abec15c28714d16c9b5145bce83162e9
philguse <philhoang.guse@outlook.de>
2019-10-16 16:31:49 +0200

    Complete part two of day 01

--- src/main/scala/Day01/ChronalCalibration.scala
+++ src/main/scala/Day01/ChronalCalibration.scala
@@ -1,12 +1,15 @@
 package Day01
 
+import scala.annotation.tailrec
 import scala.io.Source
 
 object ChronalCalibration {
   def main(args: Array[String]): Unit = {
     val frequencies = readFile("src/main/scala/Day01/input.txt")
-    val result = calculateFrequency(frequencies)
-    println(result)
+    val frequency = calculateFrequency(frequencies)
+    val frequencyTwice = calculateFrequencyTwice(frequencies)
+    println("Part one: " + frequency)
+    println("Part two: " + frequencyTwice)
   }
 
   def readFile(filename: String): List[String] = {
@@ -18,4 +21,15 @@ object ChronalCalibration {
 
   def calculateFrequency(list: List[String]): Int =
     list.foldLeft(0){_ + _.toInt}
+
+  def calculateFrequencyTwice(list: List[String]): Int = {
+    @tailrec
+    def loop(reachedFrequencies: Set[Int], acc: Int, index: Int): Int = {
+      if (reachedFrequencies.contains(acc)) acc
+      else if (index + 1 < list.length) loop(reachedFrequencies + acc, acc + list(index).toInt, index + 1)
+      else loop(reachedFrequencies + acc, acc + list(index).toInt, 0)
+    }
+
+    loop(Set(), 0, 0)
+  }
 }
