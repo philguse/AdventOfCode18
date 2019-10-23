98d6b492d3487b2327bfb2a5c49c74463a76468f
philguse <philhoang.guse@outlook.de>
2019-10-23 14:20:24 +0200

    Refactor solution for day 03

--- src/main/scala/Day03/FabricOverlapping.scala
+++ src/main/scala/Day03/FabricOverlapping.scala
@@ -3,7 +3,7 @@ package Day03
 import scala.annotation.tailrec
 import scala.io.Source
 
-case class Claim(id: String, x: Int, y: Int, width: Int, height: Int)
+case class Claim(id: String, x1: Int, y1: Int, x2: Int, y2: Int)
 
 object FabricOverlapping {
   def main(args: Array[String]): Unit = {
@@ -38,7 +38,7 @@ object FabricOverlapping {
       val position = extractValues(values(0), ",")
       val measurements = extractValues(values(1), "x")
 
-      Claim(id, position._1, position._2, measurements._1, measurements._2)
+      Claim(id, position._1, position._2, position._1 + measurements._1 - 1, position._2 + measurements._2 - 1)
     })
   }
 
@@ -48,8 +48,8 @@ object FabricOverlapping {
       case Nil => result
       case claim :: rest =>
         val newSquares = for {
-          x <- claim.x until claim.x + claim.width
-          y <- claim.y until claim.y + claim.height
+          x <- claim.x1 to claim.x2
+          y <- claim.y1 to claim.y2
         } yield (x, y)
 
         loop(rest, squares ++ newSquares, result ++ squares.intersect(newSquares.toSet))
@@ -73,15 +73,11 @@ object FabricOverlapping {
   }
 
   private def areOverlapping(a: Claim, b: Claim): Boolean = {
-    val aSquares = for {
-      x <- a.x until a.x + a.width
-      y <- a.y until a.y + a.height
-    } yield (x, y)
-    val bSquares = for {
-      x <- b.x until b.x + b.width
-      y <- b.y until b.y + b.height
-    } yield (x, y)
+    val xRange = a.x1 to a.x2
+    val yRange = a.y1 to a.y2
+    val xInRange = (b.x1 to b.x2).exists(xRange.contains)
+    val yInRange = (b.y1 to b.y2).exists(yRange.contains)
 
-    aSquares.intersect(bSquares).nonEmpty
+    xInRange && yInRange
   }
 }
